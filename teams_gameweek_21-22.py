633f334a745eaac5ecd9621a0ae0ff4edd73839e
hasan.shami <hsn.shami@gmail.com>
2022-06-07 21:02:50 +0300

    Added title to visualization + smoother

--- teams_gameweek_21-22.py
+++ teams_gameweek_21-22.py
@@ -5,7 +5,6 @@ df['Result_Home'] = df['Result'].apply(lambda x: str(x).split('-')[0]).astype(in
 df['Result_Away'] = df['Result'].apply(lambda x: str(x).split('-')[1]).astype(int)
 
 
-
 def get_points_home(x):
     if x['Result_Home'] > x['Result_Away']:
         return 3
@@ -22,19 +21,19 @@ def get_points_away(x):
         return 1
     else:
         return 3
+
+
 df['Points Home'] = df.apply(lambda x: get_points_home(x), axis=1)
 df['Points Away'] = df.apply(lambda x: get_points_away(x), axis=1)
 
+df2 = df.pivot_table(index='Round Number', columns=['Home Team', 'Away Team'], values=['Points Home', 'Points Away'],
+                     aggfunc=sum)
 
-df2 = df.pivot_table(index='Round Number', columns=['Home Team','Away Team'], values = ['Points Home', 'Points Away'], aggfunc = sum)
+dfHome = df[['Round Number', 'Date', 'Home Team', 'Points Home']]
+dfAway = df[['Round Number', 'Date', 'Away Team', 'Points Away']]
 
-dfHome = df[['Round Number','Date','Home Team','Points Home']]
-dfAway = df[['Round Number','Date','Away Team','Points Away']]
-
-dfHome.rename(columns={"Home Team": 'Team',"Points Home": 'Points'},inplace = True)
-dfAway.rename(columns={"Away Team": 'Team',"Points Away": 'Points'}, inplace= True)
+dfHome.rename(columns={"Home Team": 'Team', "Points Home": 'Points'}, inplace=True)
+dfAway.rename(columns={"Away Team": 'Team', "Points Away": 'Points'}, inplace=True)
 df_master = dfHome.append(dfAway)
-output = df_master.pivot_table(index='Round Number',columns = 'Team', values = 'Points', aggfunc=sum)
-outputDate = df_master.pivot_table(index='Date',columns = 'Team', values = 'Points', aggfunc=sum)
-
-import bar_chart_race as bcr
+output = df_master.pivot_table(index='Round Number', columns='Team', values='Points', aggfunc=sum)
+outputDate = df_master.pivot_table(index='Date', columns='Team', values='Points', aggfunc=sum)
--- visualize.py
+++ visualize.py
@@ -1,5 +1,37 @@
 import bar_chart_race as bcr
 import pandas as pd
-df = pd.read_excel('Output2.xlsx',index_col='Date')
 
-bcr.bar_chart_race(df=df, n_bars=6)
+df2 = pd.read_excel('Output2.xlsx', index_col='Date')
+df2 = df2.fillna('0')
+df2 = df2.apply(pd.to_numeric)
+df2.reset_index(inplace=True)
+df2['Date'] = df2['Date'].dt.strftime('%Y-%m-%d')
+df2.set_index('Date', inplace=True)
+df2 = df2.groupby('Date').sum()
+df3 = df2.copy()
+
+for i in df2.columns:
+    max = 0
+    index = 0
+    for j in df3[i]:
+        max = max + int(j)
+        df3[i].iloc[index] = max
+        index = index + 1
+
+# df3.apply(pd.to_numeric)
+
+
+x2 = pd.date_range(start='2021-08-12', end='2022-05-22', freq='D')
+
+df3.index = pd.DatetimeIndex(df3.index)
+df4 = df3.reindex(x2, method='ffill')
+df4.reset_index(inplace=True)
+df4['index'] = df4['index'].dt.strftime('%B %d')
+df4.set_index('index', inplace=True)
+#
+df4.to_excel("Points_By_Date.xlsx")
+bcr.bar_chart_race(df=df4, n_bars=20,
+                   filename="PL_Viz.mp4", period_length=80,
+                   fixed_max=True, steps_per_period=10,
+                   title = "English Premier League 2021-22",
+                   )
